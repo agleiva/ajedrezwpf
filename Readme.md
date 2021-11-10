433dd7b6ebfe54b752468516928a3fa5e34711c4
agleiva <agustinleiva689@gmail.com>
2021-11-10 01:55:58 -0300

    Actualizacion a .NET 6.0

--- Readme.md
+++ Readme.md
@@ -1,25 +1,29 @@
-# Ajedrez en C# y WPF
+# Ajedrez en C#
 
-![Demo](Demo2.gif "Demo")
+![Demo](Doc/Demo2.gif "Demo")
 
-## Juego completo de Ajedrez implementado en SOLO 325 líneas de código C# 8.0 y 177 líneas de XAML usando WPF corriendo sobre .NET Core 3.0
+## Juego completo de Ajedrez implementado en TAN SOLO **275** líneas de código C# y 178 líneas de XAML usando WPF corriendo sobre .NET 6.0
 
 ## Características:
 
   - Dos jugadores (Negras y Blancas)
   - Reglas básicas de movimientos de todas las piezas:
-     - Peón
-     - Alfil
-     - Caballo
-     - Torre
-     - Reina
-     - Rey
+     - `Peón`
+     - `Alfil`
+     - `Caballo`
+     - `Torre`
+     - `Reina`
+     - `Rey`
   - Marca en verde los casilleros válidos para mover
   - Historial de Movimientos (Panel Izquierdo)
   - Contador de piezas para cada jugador (Panel Derecho)
   - Se ajusta a la resolución de pantalla ("Responsive")
-  - Menú principal con 4 botones. El cargar/guardar todavía no está implementado, pero es trivial de hacer, ya que de hecho la lógica para leer los datos de un archivo ya está funcionando y se utiliza al iniciar una nueva partida para leer los datos del archivo "Default.brd"
-  - **SOLO 325 LINEAS DE C#**. 
+  - Menú principal con 4 botones.
+    - `Nueva Partida`: inicia una nueva partida
+    - `Cargar`: continuar una partida guardada
+    - `Guardar`: guardar una partida para continuarla más tarde.
+    - `Salir`: finaliza el juego.
+  - **SOLO 275 LINEAS DE C#**. 
   
 ### Detalles Visuales / UX:
   
@@ -29,14 +33,11 @@
   - La construcción del tablero y el posicionamiento de las piezas al iniciar la partida se hace de forma "animada", agregando las piezas una a una de forma aleatoria y con un mini-delay de 50 milisegundos. Esto da una sensación de animación al tablero.
   - Estilos visuales con temas (MahApps.Metro). Cambiando una sola linea de XAML puedo darle diferentes estilos/temas por color:
   
-![Cobalt](Cobalt.png "Cobalt")
+![Cobalt](Doc/Cobalt.png "Cobalt")
 
-![Dark-Blue](Dark-Blue.png "Dark-Blue")
+![Dark-Blue](Doc/Dark-Blue.png "Dark-Blue")
 
 ## Faltantes:
 
   - lógica de jaque.
-  - ciclo de juego (Nuevo juego, guardar, cargar, etc).
   - Notación Algebraica.
-  
-  
---
+++ Src/MainWindow.xaml.cs
@@ -0,0 +1,275 @@
+﻿using System.Collections.ObjectModel;
+using System.IO;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using GalaSoft.MvvmLight;
+using MvvmGen;
+using Ookii.Dialogs.Wpf;
+
+namespace AjedrezWpf;
+
+public partial class MainWindow
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+        DataContext = MainMenu.Instance;
+    }
+}
+
+[ViewModel]
+public partial class MainMenu
+{
+    public static readonly MainMenu Instance = new();
+
+    [Property] private bool isOpen = true;
+    [Property] private ChessBoard chessBoard = new();
+
+    [Command] private async Task NewGame()  => await LoadBoard("NewGame.json");
+    [Command] private async Task LoadGame() => await LoadBoard();
+
+    private async Task LoadBoard(string fileName = null)
+    {
+        if (fileName == null)
+        {
+            var dialog = new VistaOpenFileDialog { Filter = "json chess board|*.json" };
+            if (dialog.ShowDialog() != true)
+                return;
+            fileName = dialog.FileName;
+        }
+
+        var boardData = await File.ReadAllTextAsync(fileName);
+
+        IsOpen = false;
+
+        ChessBoard = JsonSerializer.Deserialize<ChessBoard>(boardData);
+        await ChessBoard.RefreshPieceCount();
+    }
+
+    [Command(nameof(CanSave))]
+    private async Task SaveGame()
+    {
+        var dialog = new VistaSaveFileDialog { DefaultExt = ".json", Filter = "json chess board|*.json" };
+        if (dialog.ShowDialog() != true)
+            return;
+
+        var boardData = JsonSerializer.Serialize(ChessBoard);
+        await File.WriteAllTextAsync(dialog.FileName, boardData);
+    }
+
+    [CommandInvalidate(nameof(ChessBoard))] 
+    private bool CanSave() => ChessBoard.Squares.Any();
+
+    [Command] private void Exit() => System.Windows.Application.Current.Shutdown();
+}
+
+public class ChessBoard : ViewModelBase
+{
+    public ObservableCollection<Square> Squares { get; set;  } = new();
+    public ObservableCollection<Movimiento> Movimientos { get; set; } = new();
+    public ObservableCollection<PieceCount> BlackPieces { get; set; } = new();
+    public ObservableCollection<PieceCount> WhitePieces { get; set; } = new();
+
+    private bool blackPlays;
+    public bool BlackPlays
+    {
+        get => blackPlays;
+        set
+        {
+            Set(ref blackPlays, value);
+            RaisePropertyChanged(() => WhitePlays);
+        }
+    }
+
+    public bool WhitePlays => !BlackPlays && !MainMenu.Instance.IsOpen;
+
+    private Square selectedSquare;
+    public Square SelectedSquare
+    {
+        get => selectedSquare;
+        set
+        {
+            OnSelectedSquareChanged(selectedSquare, value);
+            Set(ref selectedSquare, value);
+        }
+    }
+
+    public async Task RefreshPieceCount()
+    {
+        WhitePieces.Clear();
+        BlackPieces.Clear();
+        var pieces =
+            Squares.Select(x => x.Piece)
+                   .Where(x => x != null)
+                   .GroupBy(x => new { x.IsBlack, x.Type })
+                   .Select(p => (p.Key.IsBlack, p.Key.Type, p.Count()))
+                   .OrderBy(x => x.Item2);
+
+        foreach (var p in pieces)
+            (p.IsBlack ? BlackPieces : WhitePieces).Add(new PieceCount(Type: p.Type, Count: p.Item3, ImageSource: new Piece(p.IsBlack, p.Item2).ImageSource ));
+    }
+
+    public async Task OnSelectedSquareChanged(Square previousSquare, Square newSquare)
+    {
+        void ClearMoveTargets()
+        {
+            foreach (var s in Squares)
+                s.IsValidMoveTarget = false;
+        }
+
+        if (newSquare?.IsValidMoveTarget ?? false)
+        {
+            Movimientos.Add(new Movimiento(previousSquare.Piece, previousSquare, newSquare));
+
+            newSquare.Piece = previousSquare.Piece;
+            previousSquare.Piece = null;
+
+            ClearMoveTargets();
+            BlackPlays = !BlackPlays;
+            
+            await RefreshPieceCount();
+        }
+        else
+        {
+            ClearMoveTargets();
+
+            if (newSquare?.Piece?.IsBlack == BlackPlays)
+                foreach (var v in GetValidMoveTargets(newSquare))
+                    v.IsValidMoveTarget = true;
+        }
+    }
+
+    private IEnumerable<Square> GetValidMoveTargets(Square square)
+    {
+        Square MoveFrom(Square s, int v, int h) =>
+            Squares.FirstOrDefault(x => x.Row == s.Row + v && x.Col == s.Col + h);
+
+        Square Move(int v, int h) => MoveFrom(square, v, h);
+
+        bool Captures(Square sq) => sq?.Piece != null && sq.Piece.IsBlack != square.Piece.IsBlack;
+
+        IEnumerable<Square> Traverse(IEnumerable<(int r, int c)> paths)
+        {
+            foreach (var p in paths)
+            {
+                var current = square;
+                while (true)
+                {
+                    current = MoveFrom(current, p.r, p.c);
+                    if (current == null)
+                        break;
+                    else if (Captures(current))
+                    {
+                        yield return current;
+                        break;
+                    }
+                    else if (current != null && current.Piece == null)
+                        yield return current;
+                    else if (current?.Piece?.IsBlack == square.Piece.IsBlack)
+                        break;
+                    if (square.Piece.Type == PieceTypes.Rey)
+                        break;
+                }
+            }
+        }
+
+        var Cartesian = (int[] a, int[] b) => from r in a from c in b select (r, c);
+
+        switch (square.Piece.Type)
+        {
+            case PieceTypes.Peón:
+                {
+                    var v = square.Piece.IsBlack ? 1 : -1;
+
+                    var straight = Move(v, 0);
+                    if (straight.Piece == null)
+                        yield return straight;
+
+                    var isDefaultPosition =
+                        square.Piece.IsBlack && square.Row == 2 ||
+                        !square.Piece.IsBlack && square.Row == 7;
+
+                    if (isDefaultPosition)
+                    {
+                        var straight2 = Move(v * 2, 0);
+                        if (straight2.Piece == null)
+                            yield return straight2;
+                    }
+
+                    foreach (var m in new[] { 1, -1 }.Select(x => Move(v, x)).Where(Captures))
+                        yield return m;
+                }
+
+                break;
+            case PieceTypes.Torre:
+                {
+                    var paths = new[] { (1, 0), (-1, 0), (0, 1), (0, -1) };
+
+                    foreach (var s in Traverse(paths))
+                        yield return s;
+                }
+                break;
+            case PieceTypes.Caballo:
+                {
+                    var moves =
+                        new[] { (-1, -2), (-1, 2), (1, -2), (1, 2), (-2, -1), (-2, 1), (2, -1), (2, 1) }
+                        .Select(x => Move(x.Item1, x.Item2))
+                        .Where(x => x != null && (x.Piece == null || x.Piece.IsBlack != square.Piece.IsBlack));
+
+                    foreach (var m in moves)
+                        yield return m;
+                }
+                break;
+            case PieceTypes.Alfil:
+                {
+                    var paths = Cartesian(new[] { -1, 1 }, new[] { -1, 1 });
+
+                    foreach (var s in Traverse(paths))
+                        yield return s;
+                }
+                break;
+            case PieceTypes.Reina:
+                {
+                    var paths = Cartesian(new[] { -1, 0, 1 }, new[] { -1, 0, 1 });
+
+                    foreach (var s in Traverse(paths))
+                        yield return s;
+                }
+                break;
+            case PieceTypes.Rey:
+                {
+                    var paths = Cartesian(new[] { -1, 0, 1 }, new[] { -1, 0, 1 });
+
+                    foreach (var s in Traverse(paths))
+                        yield return s;
+                }
+                break;
+        }
+
+    }
+}
+
+[ViewModel]
+public partial class Square
+{
+    public int Row { get; set; }
+    public int Col { get; set; }
+    public bool IsBlack { get; set; }
+
+    [Property] private bool isValidMoveTarget;
+    [Property] private Piece piece;
+
+    [JsonIgnore]
+    public string Name => $"{"abcdefgh"[Col - 1]}{9 - Row}";
+}
+
+public record Piece(bool IsBlack, PieceTypes Type)
+{
+    public string ImageSource => "Img/" + (IsBlack ? "Negro-" : "Blanco-") + Type.ToString() + ".png";
+}
+
+public enum PieceTypes { Peón, Torre, Caballo, Alfil, Reina, Rey }
+
+public record Movimiento(Piece Piece, Square From, Square To);
+
+public record PieceCount(PieceTypes Type, int Count, string ImageSource);
